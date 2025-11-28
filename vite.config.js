c0c9c61a9e81bddb8d1330a54884ba732fca33af
workofowns <workofowns@gmail.com>
2025-11-28 22:16:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',   // ⭐ REQUIRED for Hostinger + root domain deployment
   plugins: [
     react({
       babel: {
